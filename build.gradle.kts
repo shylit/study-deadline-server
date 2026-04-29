7954a7f50df35beb93d5a1d815a90d544c848800
shylit <trisvetovrichard@gmail.com>
2026-04-29 19:11:27 +0300

    chore: создать базовый серверный проект

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "2.3.20"
+    application
+}
+
+group = "ru.mirea.shylit"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+val ktorVersion = "3.4.0"
+val logbackVersion = "1.5.18"
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("ru.mirea.shylit.studydeadline.ApplicationKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,28 @@
+package ru.mirea.shylit.studydeadline
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(
+        Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Study Deadline Server запущен")
+        }
+
+        get("/health") {
+            call.respondText("OK")
+        }
+    }
+}
