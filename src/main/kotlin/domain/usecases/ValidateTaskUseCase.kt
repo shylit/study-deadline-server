6b01d53c81d522f1729e914140253224cc2e7cc6
shylit <trisvetovrichard@gmail.com>
2026-05-02 12:34:27 +0300

    feat: добавить проверку формата дедлайна

--- src/main/kotlin/domain/usecases/ValidateTaskUseCase.kt
+++ src/main/kotlin/domain/usecases/ValidateTaskUseCase.kt
@@ -1,5 +1,8 @@
 package ru.mirea.shylit.studydeadline.domain.usecases
 
+import java.time.LocalDate
+import java.time.format.DateTimeParseException
+
 class ValidateTaskUseCase {
 
     operator fun invoke(
@@ -13,7 +16,17 @@ class ValidateTaskUseCase {
             description.isBlank() -> "Описание задания не может быть пустым"
             subject.isBlank() -> "Предмет задания не может быть пустым"
             deadline.isBlank() -> "Дедлайн задания не может быть пустым"
+            !isValidDate(deadline) -> "Дедлайн должен быть указан в формате YYYY-MM-DD"
             else -> null
+        }
+    }
+
+    private fun isValidDate(value: String): Boolean {
+        return try {
+            LocalDate.parse(value)
+            true
+        } catch (exception: DateTimeParseException) {
+            false
         }
     }
 }
